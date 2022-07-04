d1446e697a16f609055f854b83dc20fbd6f04b06
Denis Kamotsky <kamotskyd@corning.com>
2022-07-04 12:33:24 +0300

    Making packages.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pytorch-zoom-in-network',
-    packages=find_packages(exclude=['tests', 'tests.*']),
+    packages=find_packages(exclude=['data']),
     setup_requires=['wheel'],
     version='0.0.1',
     description="Efficient Classification of Very Large Images with Tiny Objects.",
