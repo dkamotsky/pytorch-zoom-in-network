6b093f3f1dfd072d0f09adccd1a8e9da7505bb9d
Denis Kamotsky <kamotskyd@corning.com>
2022-07-04 17:19:33 +0300

    freeze pytorch

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ setup(
         'opencv-python>=4.5,<4.6',
         'scikit-image==0.19.3',
         'onnx<1.9',
-        'pytorch2keras==0.2.4'
+        'pytorch2keras==0.2.4',
+        'pytorch-gpu==1.6.0',
+        'torchvision==0.7.0'
     ],
     version='0.0.1',
     description="Efficient Classification of Very Large Images with Tiny Objects.",
